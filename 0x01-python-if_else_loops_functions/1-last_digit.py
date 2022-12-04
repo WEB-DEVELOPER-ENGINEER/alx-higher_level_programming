bec3f1b093dd83ef58a766d5c5055b1be9a6de69
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-04 13:52:29 -0800

    done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,4 +13,4 @@ if x > 5:
 elif x == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, x))
 elif x < 6 and x != 0:
-    print(f"Last digit of {number:d} is {x:d} and is less than 6 and not 0")
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, x))
