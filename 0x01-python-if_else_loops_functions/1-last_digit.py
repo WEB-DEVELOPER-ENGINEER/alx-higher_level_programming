6ca04999bc58d5579e0f0806c02c0ed1cb3d84c3
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-04 14:10:08 -0800

    done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,8 @@ number = random.randint(-10000, 10000)
 n = str(number)
 for i, j in enumerate(n):
     if i > 0 and n[0] == '-':
-        x = -abs(int(n[i]))
+        x = int(n[i])
+        x = -abs(x)
     elif i > 0 and n[0] != '-':
         x = int(n[i])
 if x > 5:
