fab7432b5616bf5dab61a367e934426478114047
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-04 11:49:27 -0800

    done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ for i, j in enumerate(n):
     else:
         x = int(n[i])
 if x > 5:
-    print("Last digit of {} is {:d} and is greater than 5".format(number, x))
+    print("Last digit of {} is {} and is greater than 5".format(number, x))
 elif x == 0:
-    print("Last digit of {} is {:d} and is 0".format(number, x))
+    print("Last digit of {} is {} and is 0".format(number, x))
 elif x < 6 and x != 0:
-    print("Last digit of {} is {:d} and is less than 6 and not 0".format(number,x))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number,x))
