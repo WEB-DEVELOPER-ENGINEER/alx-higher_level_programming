2cd6c6e8af6dd3ee2889149ea76643cba1afd46d
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-04 17:13:00 -0800

    done

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,7 +3,7 @@
 /**
  * insert_node - adds a node at the beginning of a list
  * @head: pointer to a pointer to listint_t
- * @number: integer
+ * @n: integer
  * Return: the address of the new element or NULL if it failed
  */
 
@@ -28,10 +28,42 @@ listint_t *insert_node(listint_t **head, int n)
 void free_listint(listint_t *head)
 {
 	listint_t *dum;
+
 	while (head)
 	{
 		dum = head->next;
 		free(head);
 		head = dum;
 	}
+}
+
+/**
+ * add_nodeint_end - adds a new node at the end of a list
+ * @head: pointer to a pointer to listint_t
+ * @n: integer
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *dum;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (!(*head))
+	{
+		*head = new;
+		return (new);
+	}
+	dum = *head;
+	while (dum->next)
+	{
+		dum = dum->next;
+	}
+	dum->next = new;
+	return (new);
 }
