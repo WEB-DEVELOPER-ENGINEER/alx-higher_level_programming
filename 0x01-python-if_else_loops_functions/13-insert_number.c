ce846f9a1e92c9966c11d3fb76c73e40a38f5483
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-04 17:00:33 -0800

    done

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,14 +7,14 @@
  * Return: the address of the new element or NULL if it failed
  */
 
-listint_t *insert_node(listint_t **head, int number)
+listint_t *insert_node(listint_t **head, int n)
 {
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-	new->number = number;
+	new->n = n;
 	new->next = *head;
 	*head = new;
 	return (new);
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -15,7 +15,7 @@
  */
 typedef struct listint_s
 {
-	int number;
+	int n;
 	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
