57448309eb83df4339cd164d4d09feed93eb70cc
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-27 03:21:57 -0800

    done

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -29,7 +29,7 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            for i in range (0, self.__size):
-                for i in range (0, self.__size):
+            for i in range(0, self.__size):
+                for i in range(0, self.__size):
                     print("#", end="")
                 print("")
