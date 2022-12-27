a4a286a4fdce27aae97c6cd23e7855789ecf8a62
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-27 04:45:11 -0800

    done

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -14,7 +14,7 @@ my_square_3.my_print()
 print("--")
 try:
     my_square_4 = Square(3)
-    my_square_4.position = (-1, 5)
+    my_square_4.position = (0, 5)
 except Exception as e:
     print(e)
 
