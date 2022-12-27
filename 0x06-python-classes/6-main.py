b6835c7f07ea4b9399b85493601d4e1ae70c740b
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-27 05:36:19 -0800

    done

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -9,7 +9,7 @@ print("--")
 my_square_2 = Square(3, (1, 1))
 my_square_2.my_print()
 print("--")
-my_square_3 = Square(3, (3, 0))
+my_square_3 = Square(3, (3, "d"))
 my_square_3.my_print()
 print("--")
 try:
