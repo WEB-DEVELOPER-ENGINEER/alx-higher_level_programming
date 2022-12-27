ae218ef8e281d38d5fb92c90f4a32a74a381ac65
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-27 04:32:19 -0800

    done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,6 +46,6 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) != 2:
+        if type(value) != tuple or len(value) != 2 or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         slef.__position = value
