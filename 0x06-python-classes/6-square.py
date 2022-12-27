b3fdf9c6273d644dd04028b08a06a6acc92dda75
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-27 05:58:19 -0800

    done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,8 +11,9 @@ class Square:
     Also defines position using a tuple, which defaults (0, 0).
     '''
     def __init__(self, size=0, position=(0, 0)):
-        if type(position) != tuple or len(position) != 2 \
-                or position[1] != int:
+        if type(position) != tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] != int or position[1] != int or position[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
