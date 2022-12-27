e73a1c79f1f5a1c208a7c66e40f13e2a63fb84e2
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-27 06:21:36 -0800

    done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,54 +7,54 @@ Attribute position which takes a default (0, 0) tuple.
 
 
 class Square:
- '''A class that defines a square
- Also defines position using a tuple, which defaults (0, 0).
- '''
- def __init__(self, size=0, position=(0, 0)):
-  if type(position) != tuple or len(position) != 2 or \
+    '''A class that defines a square
+    Also defines position using a tuple, which defaults (0, 0).
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        if type(position) != tuple or len(position) != 2 or \
           not all([type(i) == int for i in position]):
-   raise TypeError("position must be a tuple of 2 positive integers")
-  self.__size = size
-  self.__position = position
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
 
- @property
- def size(self):
-  return self.__size
+    @property
+    def size(self):
+        return self.__size
 
- @size.setter
- def size(self, value):
-  if type(value) != int:
-   raise TypeError("size must be an integer")
-  if value < 0:
-   raise ValueError("size must be >= 0")
-  self.__size = value
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
- def area(self):
-  return self.__size**2
+    def area(self):
+        return self.__size**2
 
- def my_print(self):
-  if self.__size == 0:
-   print("")
-  else:
-   for i in range(self.__position[1]):
-    print("")
-   for i in range(0, self.__size):
-    x = 0
-    for i in range(0, self.__size):
-     if x == 0:
-      for i in range(0, self.__position[0]):
-       print(" ", end="")
-       x += 1
-     print("#", end="")
-    print("")
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__position[1]):
+                print("")
+            for i in range(0, self.__size):
+                x = 0
+                for i in range(0, self.__size):
+                    if x == 0:
+                        for i in range(0, self.__position[0]):
+                            print(" ", end="")
+                            x += 1
+                    print("#", end="")
+                print("")
 
- @property
- def position(self):
-  return self.__position
+    @property
+    def position(self):
+        return self.__position
 
- @position.setter
- def position(self, value):
-  if type(value) != tuple or len(value) != 2 or value[0] != int \
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2 or value[0] != int \
           or value[1] != int or value[0] <= 0 or value[1] <= 0:
-   raise TypeError("position must be a tuple of 2 positive integers")
-  self.__position = value
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
