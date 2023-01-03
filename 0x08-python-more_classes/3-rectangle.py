e8305c49762a8ba03c56b425ccdd1428ea4a9767
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-01-03 13:45:34 -0800

    done

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-Creates a Rectangle object height and width and calculates its area,
-perimeter and prints it with #
+This is the "Rectangle"  module.
+This module provides a simple Rectangle class.
 """
 
 
 class Rectangle:
+    """A Rectangle class with attributes width and height, and
+    methods area, perimeter, print, and str.
+    """
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         return self.__width
-
-    @property
-    def height(self):
-        return self.__height
 
     @width.setter
     def width(self, value):
@@ -26,6 +26,10 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        return self.__height
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
@@ -34,6 +38,16 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    def __str__(self):
+        total = ""
+        if self.__height == 0 or self.width == 0:
+            return total
+        for i in range(self.__height):
+            total += ("#" * self.__width)
+            if i is not self.__height - 1:
+                total += "\n"
+        return total
+
     def area(self):
         return self.__width * self.__height
 
@@ -41,9 +55,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (2 * self.__width) + (2 * self.__height)
-
-    def __str__(self):
-        r = ""
-        if self.__width != 0 and self.__height != 0:
-            r += "\n".join("#" * self.__width for i in range(self.__height))
-        return r
