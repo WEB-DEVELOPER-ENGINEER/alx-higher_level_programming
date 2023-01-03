427141e11ba36e6af05a8594ef120f6d32688d0e
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-01-03 13:39:24 -0800

    done

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -7,8 +7,6 @@ perimeter and prints it with #
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-        """Initializes a rectangle
-        """
         self.__width = width
         self.__height = height
 
