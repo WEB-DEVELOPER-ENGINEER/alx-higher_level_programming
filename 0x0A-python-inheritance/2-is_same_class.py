23e405fe55aac9e5f74af25d4d68ad13ce40ba87
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-01-09 16:28:58 -0800

    done

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' a module
+'''
+
+
+def is_same_class(obj, a_class):
+    """function: is_same_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    """
+    return type(obj) == a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' a module
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''function: is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    '''
+    return isinstance(obj, a_class)
