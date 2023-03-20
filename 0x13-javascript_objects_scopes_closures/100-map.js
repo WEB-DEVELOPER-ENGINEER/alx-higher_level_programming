6545a292dd7391906962271453c065af919805f5
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-03-20 03:44:42 -0700

    done

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-const map1 = list.map(x => list.indexOf(x) * x);
+const map1 = list.map(function (x) {
+  return (x * list.indexOf(x));
+});
 console.log(list);
 console.log(map1);
