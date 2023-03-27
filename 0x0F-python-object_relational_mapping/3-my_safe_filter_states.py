2beda8fcaa31995e27241e63f409a55a2979ac24
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-03-27 05:29:15 -0700

    done

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-
+"""
+Displays all values in the states
+where `name` matches the argument.
+Safe from SQL injections!
+"""
 
 import MySQLdb
 from sys import argv
-if __name__ == "__main__":
-    cont = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = cont.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-        (argv[4],)
-        )
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
+
+if __name__ == '__main__':
+    """
+    Access the database and get the states
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT * FROM states WHERE name LIKE BINARY %(name)s
+            ORDER BY states.id ASC """, {'name': argv[4]})
+        rows = cur.fetchall()
+    if rows is not None:
+        for row in rows:
+            print(row)
