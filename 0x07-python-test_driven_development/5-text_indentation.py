05a27a177c3e807060ae50c1719a346f6ad51a93
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-18 11:23:46 -0700

    done

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,17 +11,19 @@ def text_indentation(text):
     if type(text) != str:
         raise TypeError("text must be a string")
     lines = []
-    for line in text.splitlines():
-        line = line.strip()
+    line = ""
+    for i in range(len(text)):
+        if text[i] in [".", "?", ":"]:
+            lines.append(line.strip() + text[i])
+            line = ""
+            special_char = True
+        else:
+            line += text[i]
+            special_char = False
+    if line:
+        lines.append(line.strip())
+    for i, line in enumerate(lines):
         if line:
-            lines.append(line)
-    text = "".join(lines)
-    buff = ""
-    for c in text:
-        buff += c
-        if c in [".", "?", ":"]:
-            print(buff.strip())
-            print()
-            buff = ""
-    if buff:
-        print(buff.strip())
+            print(line, end="")
+            if i < len(lines) - 1 or special_char:
+                print("\n")
