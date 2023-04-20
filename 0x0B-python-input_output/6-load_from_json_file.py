7d95f67a1c4933f8128fab4eddc2009d023ed3c0
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-20 14:15:47 -0700

    done

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 
 def load_from_json_file(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
+    with open(filename, 'r') as f:
+        return json.load(f)
