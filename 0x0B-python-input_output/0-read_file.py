63164c46d4ab150994a2721b51afbdff318f9381
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-20 11:42:40 -0700

    done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Read file
+"""
+
+
+def read_file(filename=""):
+    '''
+        reads a text file (UTF8) and prints it to stdout
+    '''
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        for line in read_data:
+            print(line, end='')
