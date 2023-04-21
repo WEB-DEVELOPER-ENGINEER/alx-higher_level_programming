11b81357c50354e3624c3238612ee5d86c6fc08c
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-20 17:48:50 -0700

    done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Student to disk and reload
+"""
+
+
+class Student:
+    '''
+        a class Student that defines a student
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''
+            Instantiation with first_name, last_name and age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+            retrieves a dict representation of a Student instance
+        '''
+        odict = {}
+        if type(attrs) == list and all(isinstance(j, str) for j in attrs):
+            for i in attrs:
+                try:
+                    odict[i] = getattr(self, i)
+                except Exception:
+                    pass
+        else:
+            odict = self.__dict__
+        return odict
+
+    def reload_from_json(self, json):
+        '''
+            Public method to replace all attributes of the Student instance
+        '''
+        for key, value in json.items():
+            self.__setattr__(key, val)
