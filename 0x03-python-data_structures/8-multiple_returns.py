6a5c3059888779608cde7732dfe4df36727f80f9
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-05-19 16:16:23 -0700

    done

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""tuples"""
+
+
+def multiple_returns(sentence):
+    '''
+        a func that returns a tuple with the len of a str and its first char
+    '''
+    tup = (len(sentence), sentence[0])
+    return tup
