022748c3e303a815123ecc797cda7ca07d57b443
WEB-DEVELOPER-ENGINEER <101446375+WEB-DEVELOPER-ENGINEER@users.noreply.github.com>
2023-06-02 16:45:44 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-if (process.argv.slice(2).length > 1) {
-  console.log('Arguments found');
-} else if (process.argv.slice(2).length === 1) {
-  console.log('Argument found');
-} else {
-  console.log('No argument');
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let s;
-for (s in strings) {
-  console.log(strings[s]);
-}
