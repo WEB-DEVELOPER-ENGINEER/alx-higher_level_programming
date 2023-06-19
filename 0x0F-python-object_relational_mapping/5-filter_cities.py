78bcd96b2cda8d364b643d561ee0db16629572cd
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-06-18 17:49:35 -0700

    done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,17 +3,15 @@
 lists all cities from the database
 """
 if __name__ == "__main__":
-
     import MySQLdb
     from sys import argv
 
-    conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3])
-    cursor = conect.cursor()
-    cursor.execute("SELECT cities.name FROM cities "
-                    "JOIN states ON cities.state_id = states.id "
-                    "WHERE states.name = %s "
-                    "ORDER BY cities.id ASC", (argv[4],))
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities"
+                " JOIN states ON states.id=cities.state_id "
+                "WHERE states.name=%s COLLATE latin1_general_cs "
+                "ORDER BY cities.id", (argv[4],))
     print(", ".join([x[0] for x in cur.fetchall()]))
-    cursor.close()
-    conect.close()
