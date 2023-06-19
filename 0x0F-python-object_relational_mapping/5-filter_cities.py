c2f21928a11dd22561b12884c9036ef327720c7e
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-06-18 17:42:09 -0700

    done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,9 +10,10 @@ if __name__ == "__main__":
     conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], db=argv[3])
     cursor = conect.cursor()
-    cursor.execute("""SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id
-    WHERE states.name LIKE BINARY %(name)s
-    ORDER BY cities.id ASC""", {'name': argv[4]})
+    cursor.execute("SELECT cities.name FROM cities"
+                    "JOIN states ON cities.state_id = states.id"
+                    "WHERE states.name LIKE BINARY %(name)s"
+                    "ORDER BY cities.id ASC", {'name': argv[4]})
     print(", ".join([x[0] for x in cur.fetchall()]))
     cursor.close()
     conect.close()
