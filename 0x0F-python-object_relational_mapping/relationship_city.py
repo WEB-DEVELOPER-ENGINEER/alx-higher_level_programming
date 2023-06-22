833c7e77cc6f3ab77d6489d13cbc8c0c35cd7327
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-06-22 08:13:11 -0700

    done

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""The class definition of a City"""
 from sqlalchemy import Column, ForeignKey, Integer, String
 from relationship_state import Base
+from sqlalchemy.orm import relationship
 
 
 class City(Base):
     """City model class"""
 
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""The class definition of a State and an instance Base = declarative_base()"""
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class State(Base):
     """Class to states table"""
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete")
