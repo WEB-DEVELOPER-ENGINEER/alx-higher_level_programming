803538d42f731093b9c661c7f7eb2731cb1688ca
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-06-23 00:41:54 -0700

    done

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from relationship_state import Base, State
+from relationship_state import Base
 from relationship_city import City
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
