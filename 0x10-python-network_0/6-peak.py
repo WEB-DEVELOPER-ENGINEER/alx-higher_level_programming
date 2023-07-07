73d6481475f13800eba4be8c2c9225ecef5c7020
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-07 02:28:38 -0700

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,21 +2,27 @@
 """Finds a peak in a list of unsorted integers"""
 
 
-def divide(array, low, high):
-    """divide and conquer"""
-
-    mid = int((high + low)/2)
-    if array[mid-1] <= array[mid] >= array[mid+1]:
-        return array[mid]
-    elif array[mid] > array[mid+1]:
-        return divide(array, low, mid-1)
-    elif array[mid] < array[mid+1]:
-        return divide(array, mid+1, high)
-
-
 def find_peak(list_of_integers):
-    """Find peak of a list"""
+    """
+    Finds a peak in a list of unsorted integers.
 
-    if not list_of_integers:
+    Args:
+        list_of_integers: A list of integers.
+
+    Returns:
+        The value of a peak in the list, or None if the list is empty or None.
+    """
+
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
-    return divide(list_of_integers, 0, len(list_of_integers)-1)
+
+    lo = 0
+    hi = len(list_of_integers) - 1
+
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            lo = mid + 1
+        else:
+            hi = mid
+    return list_of_integers[lo]
