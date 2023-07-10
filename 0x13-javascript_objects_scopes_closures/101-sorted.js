30faf17bf2dae53123ab3260df2f4fba89b4e08b
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-10 04:20:05 -0700

    done

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const Dictn = {};
+for (const N in dict) {
+  if (Dictn[dict[N]] === undefined) {
+    Dictn[dict[N]] = [];
+  }
+  Dictn[dict[N]].push(N);
+}
+console.log(Dictn);
