5aaf44d0a32f49582bd2c305181e422c13e9b81a
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-08-03 04:06:33 -0700

    done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,8 +4,8 @@ const request = require('request');
 let i = 0;
 request(process.argv[2], (error, response, body) => {
   if (error) console.error(error);
-  const films = JSON.parse(body);
-  for (const film of films.results) {
+  const films = JSON.parse(body).results;
+  for (const film of films) {
     if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
       i += 1;
     }
